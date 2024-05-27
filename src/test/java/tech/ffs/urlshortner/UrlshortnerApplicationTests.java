bd02e9331b1ea3bb4b08c15e45ca2e7890d3c7bb
Fábio Siqueira <fabio.siqueira@axians.com>
2024-05-27 20:55:25 +0100

    Initial commit

---
+++ src/test/java/tech/ffs/urlshortner/UrlshortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ffs.urlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
