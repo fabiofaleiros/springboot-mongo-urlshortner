7ab8fd66a293d3068f74456dbd13f50457d6a569
Fábio Siqueira <fabiofaleiros@gmail.com>
2024-05-27 22:25:14 +0100

    feat(backend): refactoring package and biz rule

--- src/main/java/tech/ffs/urlshortner/controller/UrlShortnerController.java
+++ src/main/java/tech/ffs/urlshortner/controller/UrlShortnerController.java
@@ -1,26 +1,30 @@
-package controller;
+package tech.ffs.urlshortner.controller;
 
-import dto.UrlRequestDTO;
-import entities.UrlEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import repository.UrlRepository;
+import org.springframework.web.bind.annotation.*;
+import tech.ffs.urlshortner.dto.UrlRequestDTO;
+import tech.ffs.urlshortner.dto.UrlResponseDTO;
+import tech.ffs.urlshortner.entities.UrlEntity;
+import tech.ffs.urlshortner.repository.UrlRepository;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 
+@RestController
 public class UrlShortnerController {
 
     private final UrlRepository urlRepository;
 
-    public UrlController(UrlRepository urlRepository) {
+    public UrlShortnerController(UrlRepository urlRepository) {
         this.urlRepository = urlRepository;
     }
 
-    @PostMapping("/shorten-url")
-    public ResponseEntity<Void> urlShortner(@RequestBody UrlRequestDTO request,
+    @PostMapping(value = "/shorten-url")
+    public ResponseEntity<UrlResponseDTO> urlShortner(@RequestBody UrlRequestDTO request,
                                             HttpServletRequest servletRequest){
 
         String id;
@@ -32,7 +36,7 @@ public class UrlShortnerController {
 
         var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
 
-        return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
+        return ResponseEntity.ok(new UrlResponseDTO(redirectUrl));
 
     }
 
